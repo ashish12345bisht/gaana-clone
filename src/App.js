8ba7809983717a92bcf9409243febb359c2ae44b
Ashish Bisht <ashish12345bisht@gmail.com>
2022-05-22 11:48:24 +0530

    browser router updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/directory-name">
+      <BrowserRouter>
         <Header />
         <Routes>
           <Route index path="/" element={<Homepage />} />
